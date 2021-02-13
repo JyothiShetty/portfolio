f358fa1f1de2f810975e6463c9b559f5a0767011
JyothiShetty <jyothibr42@gmail.com>
2021-02-13 23:43:20 +0530

    nav bar fixed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Col, Row, Form, Button } from 'react-bootstrap'
+import { Col, Row, Form, Button, Container, Card } from 'react-bootstrap'
 
 
 function Contact() {
     return (
-        <div className="container-fluid bg-dark">
+        <div className="container-fluid bg-dark" id="contact">
+            
            <Row>
                <Col>
                 <h1 className="text-center m-4 text-light">Contact Me</h1>
                </Col>
             </Row>
-
             <Col sm="12" lg="8" md="8" className="container-fluid text-light">
             <Form action="https://formspree.io/f/xeqprpjk" method="POST">
                 <Form.Group controlId="formBasicName">
@@ -25,15 +25,14 @@ function Contact() {
 
                 <Form.Group controlId="formBasicMessage">
                     <Form.Label>Enter Message</Form.Label>
-                    <Form.Control type="message" name="message" placeholder="Enter message here" />
+                    <Form.Control as="textarea" type="message" name="message" placeholder="Enter message here"  rows={3} />
                 </Form.Group>
 
-                <Button variant="dark" value="Send" type="submit">
-                    Submit
+                <Button variant="primary" value="Send" type="submit">
+                    Send
                 </Button>
             </Form>
             </Col>
-          
         </div>
     )
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import { Button, Navbar,Nav,Form,FormControl} from 'react-bootstrap';
+import Logo from '../assests/icons/logo.png';
+import '../styles/nav.css';
 
-function Navigation() {
+const Navigation = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home"></Navbar.Brand>
-                <Nav className="ml-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
-                    <Nav.Link href="#resume">Resume</Nav.Link>
-                </Nav>
+            <Navbar fixed="top" expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between" >
+            <div>
+                <Navbar.Brand href="#home">
+                <img classname="logo" src={Logo} alt="My Portfolio Logo" width="30" height="30" />
+                </Navbar.Brand>
+            </div>
+            <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#certificates">Certificates</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
             </Navbar>
         </div>
-    )
-}
+    );
+};
 
-export default Navigation
+export default Navigation;
